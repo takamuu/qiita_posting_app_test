3b4b192a2a96f4096b09a3d0f04315abc28fcb9b
takashi <mr.takamuu@gmail.com>
2021-06-06 16:30:02 +0900

    Userモデル、マイグレーション作成

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :contract, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
+end
